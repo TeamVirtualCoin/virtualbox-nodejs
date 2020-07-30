1fd43eae4fb4b4664133b0da1b36e0003ec5ea65
TeamVirtualCoin <68771288+TeamVirtualCoin@users.noreply.github.com>
2020-07-30 18:47:03 +0530

    Added Smart Contract Support

--- virtualbox.js
+++ virtualbox.js
@@ -53,4 +53,48 @@ class VirtualBox {
 			.then(bal => return bal)
 			.catch(err => return false)
 	}
+
+        static IsContract(txid) {
+                fetch(d + "/iscontract/" + String(txid)
+                        .then(res => res.json())
+                        .then(iscontract => return iscontract)
+                        .catch(err => return false)
+        }
+
+        static EstimateContractFuel(code) {
+                fetch(d + "/contractfuel/" + code)
+                        .then(res => res.json())
+                        .then(fuel => return fuel)
+                        .catch(err => return false)
+        }
+
+        static SendContract(privateKey,code) {
+                obj = {
+                        "privateKey" : privateKey,
+                        "code" : code
+                }
+                fetch(d + "/sendcontract",{
+                        method : "POST",
+                        body : JSON.stringify(obj)
+                })
+                        .then(res => res.json())
+                        .then(tx => return tx)
+                        .catch(err => return false)
+        }
+
+        static CallContract(txid,privateKey,call,maxAllowance) {
+                obj = {
+                        "txid" : txid,
+                        "privateKey" : privateKey,
+                        "call" : call,
+                        "maxAllowance" : maxAllowance
+                }
+                fetch(d + "/callcontract",{
+                        method : "POST",
+                        body : JSON.stringify(obj)
+                })
+                        .then(res => res.json())
+                        .then(tx => return tx)
+                        .catch(err => return false)
+        }
 }
